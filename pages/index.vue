bc7887f370ea83442e5c7ee77de8f7350ec64bff
Nisthar <nistharkalathilk@gmail.com>
2018-08-16 14:49:16 +0530

    first commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,18 @@
 <template>
   <section class="container">
-    <img src="~assets/img/logo.png" alt="Logo" class="logo" />
-    <h1 class="title">
-      USERS
-    </h1>
-    <ul class="users">
-      <li v-for="(user, index) in users" :key="index" class="user">
-        <nuxt-link :to="{ name: 'id', params: { id: index }}">
-          {{ user.name }}
-        </nuxt-link>
-      </li>
-    </ul>
+    <p class="title">
+      The app is working. Please test it on the messenger platform.
+    </p>
+
   </section>
 </template>
 
 <script>
-import axios from '~/plugins/axios'
 
 export default {
-  async asyncData () {
-    let { data } = await axios.get('/api/users')
-    return { users: data }
-  },
+  
   head () {
     return {
-      title: 'Users'
     }
   }
 }
